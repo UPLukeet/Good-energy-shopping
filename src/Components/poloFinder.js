3065c4dc44ad30abb848983b7664b067dac32caa
lukeet <up858296@myport.ac.uk>
2020-10-28 17:50:02 +0000

    added basic styling for background elements

---
+++ src/Components/poloFinder.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Data from "./polo-variants.json";
+
+function PoloFinder() {
+  return (
+    <div className="PF_Container">
+      <div className="PF_Box">
+        <div className="PF_Content">
+          <div className="PF_Input">
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default PoloFinder;
