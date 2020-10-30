fca4ac16cf0fe57bdaf8a948ce07b7114a82e266
lukeet <up858296@myport.ac.uk>
2020-10-30 13:12:26 +0000

    Added drop down selecters and some css styling

--- src/Components/poloFinder.js
+++ src/Components/poloFinder.js
@@ -1,16 +1,58 @@
 import React, { useState } from "react";
 import Data from "./polo-variants.json";
+import Select from "react-select";
 
 function PoloFinder() {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [series, setSeries] = useState(null);
+
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
+
+  const seriesOptions = [
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+    { value: "5", label: "5" },
+    { value: "6", label: "6" },
+  ];
+
+  const colourStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      color: state.isSelected ? 'white' : 'black',
+      background: state.isSelected ? '#51C184' : 'white',
+      padding: 20,
+    }),
+  }
+
   return (
     <div className="PF_Container">
       <div className="PF_Box">
         <div className="PF_Content">
           <div className="PF_Input"></div>
-          <label>test</label>
-          <label>test Two</label>
-          <label>test Three</label>
-          <p>shbshb</p>
+          <Select
+            className={"poloOption_container"}
+            classNamePrefix={"poloOptions"}
+            defaultValue={series}
+            onChange={setSeries}
+            options={seriesOptions}
+            styles={colourStyles}
+            placeholder={"Polo seies"}
+          />
+          <Select
+            className={"poloOption_container"}
+            classNamePrefix={"poloOptions"}
+            defaultValue={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+            styles={colourStyles}
+            placeholder={"Test2"}
+          />
         </div>
       </div>
     </div>
