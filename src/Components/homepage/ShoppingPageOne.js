8c7d51ce4a27775135601cf1033d13601606f65e
Lukeet <lukeetwebdev@gmail.com>
2020-12-06 17:29:10 +0000

    added box that switches flow direction for wider displays

--- src/Components/homepage/ShoppingPageOne.js
+++ src/Components/homepage/ShoppingPageOne.js
@@ -23,38 +23,37 @@ const ShoppingPageOne = (props) => {
   return (
     <div className={"Shopping_Content " + pageone_show}>
 
-      
 
-      <div className='Shopping_input_container'>
-        {Data.map((Ingredients) => {
+      <div className="Shopping_input_aligner">
+        <div className='Shopping_input_container'>
+          {Data.map((Ingredients) => {
 
-          //updates Quanties Hook
-          const handleChange = (event) => {
+            //updates Quanties Hook
+            const handleChange = (event) => {
 
-            setQuantities({
-              ...Quantities,
-              [Ingredients.Name]: {
-                ...(Quantities[Ingredients.Name] ?? {}),
-                quantities: event.target.value
-              }
-            });
-          };
+              setQuantities({
+                ...Quantities,
+                [Ingredients.Name]: {
+                  ...(Quantities[Ingredients.Name] ?? {}),
+                  quantities: event.target.value
+                }
+              });
+            };
 
-          return (<div className={"Shopping_input " + Ingredients.Name} key={Ingredients.Name}>
-            <p>{Ingredients.Name} £{Ingredients.Price}</p>
-            <input onChange={handleChange.bind(this)} min="0" placeholder="0" type="number"></input>
-          </div>)
-        })}
+            return (<div className={"Shopping_input " + Ingredients.Name} key={Ingredients.Name}>
+              <p>{Ingredients.Name} £{Ingredients.Price}</p>
+              <input onChange={handleChange.bind(this)} min="0" placeholder="0" type="number"></input>
+            </div>)
+          })}
+        </div>
+
+        <div className='Discount_list'>
+          <h3>Discounts:</h3>
+          <li>Buy one cheese get one free!</li>
+          <li>Buy a Soup get a half price bread!</li>
+          <li>A third off butter!</li>
+        </div>
       </div>
-
-      <div className='Discount_list'>
-        <h3>Discounts:</h3>
-        <li>Buy one cheese get one free!</li>
-        <li>Buy a Soup get a half price bread!</li>
-        <li>A third off butter!</li>
-
-      </div>
-
       <div className="Shopping_Buttons">
         <p onClick={props.next_ClickHandler}>Buy Now!</p>
       </div>
